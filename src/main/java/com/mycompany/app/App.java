5b23c367bf53a1a54bfaab292ccc7c2482d1ecae
Jannis Cloodt <jcloodt@cl-1-5.micromata.priv>
2023-09-12 14:00:33 +0200

    Natürliche Eingabe

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,40 +1,34 @@
 package com.mycompany.app;
+
 import java.util.Scanner;
 import java.lang.String;
 import java.text.DecimalFormat;
 
 
-public class App 
-{
+public class App {
 
     //Funktion zur berechnung von + - * /
-    public static float berechnen(float a, float b, int art)
-    {
+    public static float berechnen(float a, float b, int art) {
 
         float ergebnis = 0;
-        if(art == 1)
-        {
-            ergebnis = a+b;
+        if (art == 1) {
+            ergebnis = a + b;
         }
-        if(art == 2)
-        {
-            ergebnis = a-b;
+        if (art == 2) {
+            ergebnis = a - b;
         }
-        if(art == 3)
-        {
-            ergebnis = a*b;
+        if (art == 3) {
+            ergebnis = a * b;
         }
-        if(art == 4)
-        {
-            ergebnis = a/b;
+        if (art == 4) {
+            ergebnis = a / b;
         }
 
         return ergebnis;
     }
 
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         //Anlegen Objekte für Einlesen und Formatierung des Erebnisses
         DecimalFormat df = new DecimalFormat("0.00");
         Scanner sc = new Scanner(System.in);
@@ -42,44 +36,33 @@ public class App
         //Soll das Programm beendet werden?
         boolean beenden = false;
 
-        while(beenden == false)
-        {
+        while (beenden == false) {
             int eingabe = 0;
+            String zahlen = "";
 
-            //Was soll Berechnet werden?
-            System.out.println("Addition.................1");
-            System.out.println("Subtraktion..............2");
-            System.out.println("Miltiplikation...........3");
-            System.out.println("Division.................4");
-            System.out.println("Beenden..................5");
-            System.out.println("Was wollen Sie berechnen?: ");
-            eingabe = Integer.valueOf(sc.next());
-
-            //Überprüfung auf korrekte Eingabe
-            if(eingabe > 0 && eingabe < 6)
-            {
-                float a = 0;
-                float b = 0;
-
-                System.out.println("Geben Sie ihre erste Zahl ein!: ");
-                a = Float.valueOf(sc.next());
-
-                System.out.println("Geben Sie ihre zweite Zahl ein!: ");
-                b = Float.valueOf(sc.next());
-
-                System.out.println("Ergebnis: " + df.format(berechnen(a, b, eingabe)));
+            System.out.println("Werte mit operator eingaben:");
+            zahlen = sc.next();
+            if (zahlen.contains("+")) {
+                eingabe = 1;
+            } else if (zahlen.contains("-")) {
+                eingabe = 2;
+            } else if (zahlen.contains("*")) {
+                eingabe = 3;
+            } else if (zahlen.contains(":")) {
+                eingabe = 4;
+            } else {
+                continue;
             }
+
+            String[] result = zahlen.split("\\+|\\*|-|:");
+
+
+            System.out.println(berechnen(Integer.valueOf(result[0]), Integer.valueOf(result[1]), eingabe));
+            // }
 
             //Programm Beenden
-            else if(eingabe == 5)
-            {
-                beenden = true;
-            }
 
             //Wenn eine Fehlerhafte Auswahl getroffen wurde
-            else {
-                System.out.println("ERROR");
-            }
         }
     }
 }
